7a83d0cb0c082a71af296a8bbf250acac065c054
Bllinger <1760567382@qq.com>
2020-07-18 18:37:47 +0800

    update from win

---
+++ src/main/ch2/tempconv.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"tempconv"
+)
+
+func main() {
+	c := tempconv.Celsius(24)
+
+	fmt.Println(c.String())
+	fmt.Println("Celsius of Boiling:", tempconv.BoilingC)
+	fmt.Println("Fahrenheit of AbsoluteZero", tempconv.CToF(tempconv.AbsoluteZeroC))
+}
